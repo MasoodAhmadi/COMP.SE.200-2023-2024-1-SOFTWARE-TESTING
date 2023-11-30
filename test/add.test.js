dbd0925b141a91ce6ecd4566ea118ff69939f530
Masood Ahmadi <masoodahmadi553@gmail.com>
2023-12-01 00:29:42 +0200

    small modifications

--- test/add.test.js
+++ test/add.test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 describe('Test Add Operation', () => {
   it('should return 10', () => {
-    const ans = addOperations(6, 4);
+    const ans = add(6, 4);
     assert.equal(ans, 10);
   });
 });
