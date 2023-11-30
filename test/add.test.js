d122659b8c85f53b9b69963b098330283b5b9484
Masood Ahmadi <masoodahmadi553@gmail.com>
2023-12-01 00:27:08 +0200

    small modifications

--- test/add.test.js
+++ test/add.test.js
@@ -1,29 +1,10 @@
-// import add from '../src/add';
+import add from '../src/add';
 
-// describe('add', () => {
-//   it('should add two numbers', () => {
-//     expect(add(6, 4)).toBe(10);
-//   });
+const assert = require('assert');
 
-//   it('should add positive and negative numbers', () => {
-//     expect(add(5, -3)).toBe(2);
-//   });
-
-//   it('should add decimal numbers', () => {
-//     expect(add(2.5, 1.5)).toBe(4);
-//   });
-
-//   it('should handle zero as an input', () => {
-//     expect(add(1, 0)).toBe(1);
-//     expect(add(0, 5)).toBe(5);
-//   });
-
-//   it('should throw an error when non-numeric inputs are provided', () => {
-//     expect(() => add('a', 1)).toThrowError(
-//       'Invalid input: "a" is not a number'
-//     );
-//     expect(() => add(1, 'b')).toThrowError(
-//       'Invalid input: "b" is not a number'
-//     );
-//   });
-// });
+describe('Test Add Operation', () => {
+  it('should return 10', () => {
+    const ans = addOperations(6, 4);
+    assert.equal(ans, 10);
+  });
+});
